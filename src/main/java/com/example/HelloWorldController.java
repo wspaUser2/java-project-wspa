489b0dd8adffe5e2a35ba216395acc1f51e37795
coko lwiek <coko@lwiek.com>
2016-05-27 19:10:09 +0200

    Yay mamy html i bootstrapa

--- src/main/java/com/example/HelloWorldController.java
+++ src/main/java/com/example/HelloWorldController.java
@@ -1,12 +1,15 @@
 package com.example;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@RestController
+@Controller
 public class HelloWorldController {
 	@RequestMapping("/greet")
-	public String helloAction(){
-		return "Hello world";
+	public String helloAction(@RequestParam(value = "name", required = false, defaultValue="Marek") String name,Model uiModel){
+		uiModel.addAttribute("name",name);
+		return "example_file";
 	}
 }
